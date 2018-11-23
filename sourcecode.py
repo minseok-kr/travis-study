8cdcdd13df839f6abf0fd029d018c69e91a8bfa7
Minseok <iamminseokpark@gmail.com>
2018-11-23 17:02:50 +0900

    Add test code

--- sourcecode.py
+++ sourcecode.py
@@ -1,1 +1,9 @@
-print("Hello, World")
+def get_helloworld():
+	return 'hello world'
+
+if __name__ == '__main__':
+	print(get_helloworld())
+
+
+def test_get_helloworld():
+	assert 'hello world' == get_helloworld()
